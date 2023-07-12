3ad154c379bf721f0eb409fd708ee1a15fc09f46
Akarsh Gharge <akarshgharge20@gmail.com>
2023-07-12 15:44:45 -0400

    Added icons to navigation

--- src/components/shared/NavigationBar.js
+++ src/components/shared/NavigationBar.js
@@ -6,6 +6,8 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Image, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
+import { HouseFill, TrophyFill, FileLock2Fill, BookmarkCheckFill } from "react-bootstrap-icons";
+
 const lngs = [
     { code: "en", native: "English"},
     { code: "fr", native: "French" }
@@ -28,19 +30,22 @@ function NavigationBar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='m-auto'>
-                        <Nav.Link href="/" className='mx-2'>{t('home')}</Nav.Link>
+                        <Nav.Link href="/" className='mx-2'>
+                            <HouseFill size={20} className='mx-1'/>
+                            {t('home')}
+                        </Nav.Link>
                         <NavDropdown title={t('play')} className='mx-2'>
                             <NavDropdown.Item href="/horizonView">Horizon View</NavDropdown.Item>
                             <NavDropdown.Item href="/soaringHeights">Soaring Heights</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/courseBooking">{t('bookNow')}</NavDropdown.Item>
+                            <NavDropdown.Item href="/courseBooking"><BookmarkCheckFill size={20} className='mx-1'/>{t('bookNow')}</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/tournamentList" className='mx-2'>{t('compete')}</Nav.Link>
+                        <Nav.Link href="/tournamentList" className='mx-2'><TrophyFill size={20} className='mx-1'/>{t('compete')}</Nav.Link>
                         <NavDropdown title={t('learn')} className='mx-2'>
                             <NavDropdown.Item href="/learn/basics">{t('basicResources')}</NavDropdown.Item>
                             <NavDropdown.Item href="/learn/instructors">{t('instructors')}</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href='/memberLogin'>{t('memberLogin')}</Nav.Link>
+                        <Nav.Link href='/memberLogin'><FileLock2Fill size={20} className='mx-1'/>{t('memberLogin')}</Nav.Link>
                         <NavDropdown title={t('language')} className='mx-2'>
                             {lngs.map((lng, i) => {
                                 const { code, native } = lng;
